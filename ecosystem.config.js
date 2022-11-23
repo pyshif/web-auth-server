51083d326ae43a41a6c5023c2b3b61f4dd473211
Po Yu Chen <pyshif@gmail.com>
2022-11-23 18:50:36 +0800

    adjust port with production mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 "NODE_ENV": "development"
             },
             env_production: {
-                "PORT": 80,
+                "PORT": 90,
                 "NODE_ENV": "production",
             }
         }
