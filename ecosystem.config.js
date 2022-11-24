5a2d97202a104fd526b803a2732e5732269dc7a4
Po Yu Chen <pyshif@gmail.com>
2022-11-25 02:11:02 +0800

    add ignore watch list to pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,18 @@ module.exports = {
         {
             name: "web-auth-server",
             script: "./bin/www",
+            watch: true,
+            ignore_watch: [
+                'node_modules',
+                'readme',
+                'public',
+                'sessions',
+                'database',
+                'test',
+                '.env.*',
+                '.prettierrc',
+                'READEME.md'
+            ],
             env: {
                 "PORT": 3003,
                 "NODE_ENV": "development"
